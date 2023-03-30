2be8c62c548c129856185b866bc7d3726314533d
Lada Starodubtseva <e2038512@cmaisonneuve.qc.ca>
2023-03-30 12:07:43 -0400

    Ajout du menu cours

--- 404.php
+++ 404.php
@@ -7,29 +7,40 @@ $image = "foret-conifer.jpg";
 is_front_page();
 ?>
 <?php get_header(); ?>
+
 <body class="site no-aside">
-    
 
-   <section class="site__main__404__body">
 
-    <main class="site__main__404">
+    <section class="site__main__404__body">
+
+        <main class="site__main__404">
 
             <h1>404.php</h1>
             <h2>Page non disponible</h2>
-            
+
             <p>Page introuvable, Vous pouvez tenter une recherche!</p>
 
             <?= get_search_form() ?>
 
             <nav class="menu-notes-404">
-            <h2>Les notes de cours</h2>
-            <?php wp_nav_menu(array(
+                <h2>Les notes de cours</h2>
+                <?php wp_nav_menu(array(
                             "menu" => "notes",
                             "container" => "nav",
                             "container_class" => "menu__main",
                     )); ?>
- 
-    </main>
-   </section>
-    </body>
- <?php get_footer(); ?>
+
+                <h2>Nos choix de cours</h2>
+                <?php wp_nav_menu(array(
+                            "menu" => "cours",
+                            "container" => "nav",
+                            "container_class" => "menu__main",
+                    )); 
+                    
+                
+            ?>
+
+        </main>
+    </section>
+</body>
+<?php get_footer(); ?>
