468d4b73bd13e489264962b377de01b076f76d21
Lada-Starodubtseva <112096860+Lada-Starodubtseva@users.noreply.github.com>
2023-05-09 12:17:42 -0400

    mise en page des thumbnails

--- front-page.php
+++ front-page.php
@@ -6,19 +6,33 @@
 <?php get_header(); ?>
 
 <main class="site__main">
+
     <pre>front-page.php</pre>
     <h1>bienvenue sur 4w4</h1>
     <section class="blocflex">
-<?php if(have_posts()):
+        <div class="menu__evenements">
+            <p>Évenements</p>
+            <?php wp_nav_menu(array(
+            
+                        "menu" => "Evenements",
+                        "container" => "nav",
+                        "container_class" => "menu__evenements",
+                )); ?>
+
+        </div>
+    </section>
+    <section class="blocflex">
+        <?php if(have_posts()):
         while(have_posts()): the_post(); ?>
         <?php if (in_category('galerie')){ 
             get_template_part("template-parts/categorie", "galerie");}
             else{
    get_template_part("template-parts/categorie", "4w4");} ?>
         <?php endwhile; ?>
-       <?php endif;?>
-</section>
+        <?php endif;?>
+    </section>
 </main>
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
 </body>
+
 </html>
